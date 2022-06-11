645277aaba6de844c3260ea63e9685e72e554bcf
Jungle68 <335891510@qq.com>
2022-06-11 21:35:46 +0800

    feat:  pages config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,7 @@
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig ({  
+    base:'/android-note/',
     lang: 'zh-CN',
     title: 'Android Notes',
     description: '一个 Android 技术记录文档',
